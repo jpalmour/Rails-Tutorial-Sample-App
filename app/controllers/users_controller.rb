72e158602949c60d584dd547e6233a20a0dbd661
Joseph Palmour <jpalmour@gmail.com>
2013-10-26 14:20:33 -0400

    found project in this failing test state after abandoning it for a few weeks.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+
   def show 
 		@user = User.find(params[:id])
   end
 
-	def new
-		@user = User.new
+  @user = User.new
+  def new
+  end
+
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 
 	def create
@@ -22,5 +37,16 @@ class UsersController < ApplicationController
 
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		def signed_in_user
+			redirect_to signin_url, notice: "Please sign in." unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			logger.debug "in correct_user, id: #{params[:id]}"
+			logger.debug "in correct_user, id: #{@user.attributes.inspect}"
+			redirect_to(root_url) unless current_user?(@user)
 		end
 end
