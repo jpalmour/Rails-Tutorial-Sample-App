472605b91c41347f4501663010906100c6be7042
Joseph Palmour <jpalmour@gmail.com>
2013-10-29 08:18:20 -0400

    finish user edut, update, undex, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:edit, :update, :index]
+	before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
 	before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def show 
 		@user = User.find(params[:id])
@@ -35,7 +36,13 @@ class UsersController < ApplicationController
 		else
 			render 'new'
 		end
-	end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
 
 	private
 
@@ -53,5 +60,9 @@ class UsersController < ApplicationController
 		def correct_user
 			@user = User.find(params[:id])
 			redirect_to(root_url) unless current_user?(@user)
-		end
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
